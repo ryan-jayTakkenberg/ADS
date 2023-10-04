1cef9e9d1898e53a6a5bbed16798749fc962400f
Ryan-Jay Takkenberg <Ryan-jay.takkenb erg@hva.nl>
2023-10-05 00:06:26 +0200

    Detection works

--- Assignment2/src/main/java/models/Detection.java
+++ Assignment2/src/main/java/models/Detection.java
@@ -2,6 +2,7 @@ package models;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
 
 import static models.Car.CarType;
@@ -28,6 +29,7 @@ public class Detection {
      * De kentekenplaat moet overeenkomen met een auto uit de verstrekte lijst.
      * Als er geen overeenkomende auto kan worden gevonden, wordt een nieuwe Car geïnstantieerd met de opgegeven kentekenplaat en toegevoegd aan de lijst
      * (behalve het kentekenplaatnummer zal er geen andere informatie beschikbaar zijn over deze auto)
+     *
      * @param textLine
      * @param cars     een lijst van bekende auto's, geordend en doorzoekbaar op kentekenplaat
      *                 (dwz de indexOf-methode van de lijst houdt alleen rekening met de kentekenplaat bij het vergelijken van auto's)
@@ -37,28 +39,78 @@ public class Detection {
     public static Detection fromLine(String textLine, List<Car> cars) {
         Detection nieuweDetectie = null;
 
-        cars.indexOf(cars);
+        String[] detectionParts = textLine.split(",");
 
-        // TODO zet de informatie in de tekstregel om in een nieuwe Detection-instantie
-        //  gebruik cars.indexOf om de auto te vinden die is gekoppeld aan de kentekenplaat van de detectie
-        //  als er geen auto kan worden gevonden, wordt een nieuwe Car geïnstantieerd en toegevoegd aan de lijst en gekoppeld aan de detectie
+        // Zorg ervoor dat de tekstregel de verwachte indeling heeft (kentekenplaat, stad, datumTijd)
+        if (detectionParts.length == 3) {
+            String carPlate = detectionParts[0].trim();
+            String city = detectionParts[1].trim();
+            String dateTimeStr = detectionParts[2].trim();
+
+            // Probeer de datumtijd te parsen naar een LocalDateTime-object
+            LocalDateTime dateTime = null;
+            try {
+                dateTime = LocalDateTime.parse(dateTimeStr);
+            } catch (Exception e) {
+                // Als de parsing mislukt, kun je hier een foutafhandeling toevoegen
+                e.printStackTrace();
+            }
+
+            if (dateTime != null) {
+                int index = -1;
+
+                // Zoek naar een overeenkomende auto in de lijst van bekende auto's
+                for (int i = 0; i < cars.size(); i++) {
+                    if (cars.get(i).getLicensePlate().equals(carPlate)) {
+                        index = i;
+                        break;
+                    }
+                }
+
+                // Controleer of een overeenkomende auto is gevonden
+                if (index != -1) {
+                    // Een overeenkomende auto is gevonden, maak een nieuwe Detection-instantie aan
+                    nieuweDetectie = new Detection(cars.get(index), city, dateTime);
+                } else {
+                    // Geen overeenkomende auto gevonden, maak een nieuwe Car en Detection-instantie aan
+                    Car nieuweAuto = new Car(carPlate);
+                    cars.add(nieuweAuto); // Voeg de nieuwe auto toe aan de lijst van bekende auto's
+                    nieuweDetectie = new Detection(nieuweAuto, city, dateTime);
+                }
+            }
+        }
 
         return nieuweDetectie;
     }
-
 
     /**
      * Validates a detection against the purple conditions for entering an environmentally restricted zone
      * I.e.:
      * Diesel trucks and diesel coaches with an emission category of below 6 may not enter a purple zone
+     *
      * @return a Violation instance if the detection saw an offence against the purple zone rule/
-     *          null if no offence was found.
+     * null if no offence was found.
      */
     public Violation validatePurple() {
-        // TODO validate that diesel trucks and diesel coaches have an emission category of 6 or above
+        Car chosenCar = car;
+
+        if (car.getCarType() == CarType.valueOf("Truck") || car.getCarType() == CarType.valueOf("Coach")) {
+            if (car.getFuelType() == FuelType.Diesel) {
+                if (car.getEmissionCategory() >= 6) {
+                    return new Violation(chosenCar, city);
+
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+
+        }
 
 
-        return null;
     }
 
     public Car getCar() {
@@ -78,7 +130,8 @@ public class Detection {
     public String toString() {
         // TODO represent the detection in the format: licensePlate/city/dateTime
 
-        return car+"/"+city+"/"+dateTime;       // replace by a proper outcome
+        return car.getLicensePlate() + "/" + city + "/" + dateTime;       // replace by a proper outcome
     }
+
 
 }
