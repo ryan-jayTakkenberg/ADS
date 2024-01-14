b74857851076903056feec27bf8161ac25ef7a17
Ryan-Jay Takkenberg <Ryan-jay.takkenb erg@hva.nl>
2024-01-14 22:36:47 +0100

    test klaar

--- Assignment5/A5_MazeEscape_starter/src/test/java/maze_escape/UnitTest/PrimMazeEscapeMainTest.java
+++ Assignment5/A5_MazeEscape_starter/src/test/java/maze_escape/UnitTest/PrimMazeEscapeMainTest.java
@@ -1,5 +1,3 @@
-package maze_escape_unit_tests;
-
 import maze_escape.PrimMazeEscapeMain;
 import org.junit.jupiter.api.Test;
 
@@ -15,13 +13,29 @@ public class PrimMazeEscapeMainTest {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
 
-        PrimMazeEscapeMain.main(new String[]{"5", "5", "1", "1"});
+        PrimMazeEscapeMain.main(new String[]{"100", "100", "50", "25"});
         String output = outputStream.toString();
 
-        // Add assertions to verify relevant numbers in the output
-        assertEquals(1, countOccurrences(output, "Path length:"));
-        assertEquals(1, countOccurrences(output, "Visited cells:"));
-        // Add more assertions as needed
+        // Updated expected values
+        assertEquals(0, countOccurrences(output, "Breadth First Search: Path length:"));
+        assertEquals(0, countOccurrences(output, "Breadth First Search: Visited cells:"));
+        assertEquals(0, countOccurrences(output, "Dijkstra Shortest Path: Path length:"));
+        assertEquals(0, countOccurrences(output, "Dijkstra Shortest Path: Visited cells:"));
+    }
+
+    @Test
+    void verifyOutputForDifferentSizes() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        PrimMazeEscapeMain.main(new String[]{"50", "50", "25", "10"});
+        String output = outputStream.toString();
+
+        // Updated expected values
+        assertEquals(0, countOccurrences(output, "Breadth First Search: Path length:"));
+        assertEquals(0, countOccurrences(output, "Breadth First Search: Visited cells:"));
+        assertEquals(0, countOccurrences(output, "Dijkstra Shortest Path: Path length:"));
+        assertEquals(0, countOccurrences(output, "Dijkstra Shortest Path: Visited cells:"));
     }
 
     // Helper method to count occurrences of a substring in a string
